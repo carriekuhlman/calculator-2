6b7393940f841a719a422a3cf1234f2187a12565
Carrie Kuhlman <carrie.kuhlman@gmail.com>
2021-01-22 05:30:09 +0000

    Working on user input error handling. There are issues.

--- calculator.py
+++ calculator.py
@@ -13,12 +13,40 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 while True:
     input_string = input("Write an equation > ")
     tokens = input_string.split(' ')
-    valid_tokens = ["+", "-", "*", "/", "square", "cube", "pow", "mod"]  
+    valid_tokens = {"+": 3, "-": 3, "*": 3, "/": 3, "square": 2, "cube": 2, "pow": 3, "mod": 3} 
 
     if input_string == "q":
         break
 
+    # elif tokens[0] not in valid_tokens:
+    #     print("Invalid input")
+    
+    elif len(tokens) != valid_tokens[tokens[0]]:
+        print("Invalid input")
+    
+     
+    # elif tokens[0] in valid_tokens:
+    #     for i in range(1,len(tokens)):
+    #         try: 
+    #             int(tokens[i])
+    #         except:
+    #             print("Input not valid.")
+    #             break
+
+    for i in range(len(tokens)): 
+        if i == 0: 
+            if tokens[0] not in valid_tokens: 
+                print("Invalid input.")
+                break
+        else: 
+            try: 
+                int(tokens[i])
+            except: 
+                print("Input not valid")
+                break
+
     else:
+
         #create valid tokens list
         #iterate through tokens (using range of len)
             #if tokens[0] is not in our operator list
@@ -26,16 +54,6 @@ while True:
             #else:
                 #try to convert the passed string to integer
                 #exception would be a print statement with an error message
-
-        # for i in range(len(tokens)): 
-        #     if i == 0:
-        #         if tokens[i] not in valid_tokens: 
-        #             print("Input not valid.")
-        #     else:
-        #         try: 
-        #             int(tokens[i])
-        #         except:
-        #             print("Input not valid.") 
 
         if tokens[0] == "+":
             answer = (add(int(tokens[1]), int(tokens[2])))
@@ -60,6 +78,6 @@ while True:
 
         elif tokens[0] == "mod":
             answer = (mod(int(tokens[1]), int(tokens[2])))
-           
-        print(float(answer))
         
+        print(float(answer))
+            
